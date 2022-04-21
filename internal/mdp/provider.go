e647ec3caee3f38149895c30441fe583f6e12398
Mohammad Vatandoost <mohamadvatandoost512@gmail.com>
2022-04-21 16:31:13 +0430

    add mdp

--- internal/mdp/provider.go
+++ internal/mdp/provider.go
@@ -6,8 +6,13 @@ type MarkovDecisionProcess struct {
     actions [][]float32
 }
 
-func (mdp *MarkovDecisionProcess) NextState()  {
-	return
+func (mdp *MarkovDecisionProcess) NextState() int {
+	mdp.currentState = sample(mdp.actions[mdp.currentState])
+	return mdp.currentState
+}
+
+func (mdp *MarkovDecisionProcess) SetActionState(state int, action []float32)  {
+	mdp.actions[state] = action
 }
 
 func New(states []string, currentState int, actions [][]float32) *MarkovDecisionProcess {
--- internal/mdp/utility.go
+++ internal/mdp/utility.go
@@ -1,1 +1,17 @@
 package mdp
+
+import "math/rand"
+
+func sample(pdf []float32) int {
+	r := rand.Float32()
+	cdf := make([]float32, len(pdf))
+	cdf[0] = pdf[0]
+	for i := 1; i < 4; i++ {
+		cdf[i] = cdf[i-1] + pdf[i]
+	}
+	bucket := 0
+	for r > cdf[bucket] {
+		bucket++
+	}
+	return bucket
+}
